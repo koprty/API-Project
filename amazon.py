8c597b4e41bf2e06f7e053d10992cd585c213653
koprty <your_email@example.com>
2014-11-11 18:45:42 -0500

    finished blendedsearch with img link, height, and width

--- amazon.py
+++ amazon.py
@@ -1,6 +1,6 @@
 from amazonproduct import API
 import operator
-#NOTE - price formatting has not been implemented (since it screws up the sorting stuff)
+#NOTE - $ price formatting has not been implemented (since it screws up the sorting stuff)   
 config ={
     'access_key': 'AKIAIE3MWTF3ITAIP4OA',
     'secret_key': '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',
@@ -9,13 +9,7 @@ config ={
     }
 
 api = API(cfg=config)
-
-def booksearch ():
-    items = api.item_search('Books', Publisher="O'Reilly")
-    for book in items:
-        print '%s: "%s"' % (book.ItemAttributes.Author,
-                            book.ItemAttributes.Title)
-
+#returns a tuple with (name, unformatted-price-but-with-decimal-point, page-url, img-url(medium-size), height, width)
 def blendedsearch(searchwords):
     items = api.item_search('All', Keywords=searchwords)
     d=[]
@@ -27,13 +21,17 @@ def blendedsearch(searchwords):
         try:
             product = api.item_lookup(str(asin)) #itemAttributes has name, brand, and other info
             productprice = api.item_lookup(str(asin),ResponseGroup="OfferFull") #price info
-            
+            image = api.item_lookup(str(asin),ResponseGroup="Images") #image info
+ 
             name = product.Items.Item.ItemAttributes.Title
             price = productprice.Items.Item.OfferSummary.LowestNewPrice.Amount
             link = product.Items.Item.DetailPageURL
+            imglink = image.Items.Item.MediumImage.URL
+            height = image.Items.Item.MediumImage.Height
+            width = image.Items.Item.MediumImage.Width
             
-            d.append((name, float("{0:.2f}".format(price/100.0)), link)) #cannot add dollarsign or else it messes up sorting
-        except (AttributeError):
+            d.append(  (name, float("{0:.2f}".format(price/100.0)), link, imglink, height, width)   ) #cannot add dollarsign or else it messes up sorting
+        except AttributeError:
             pass
     sortedd=sorted(d, key=operator.itemgetter(1), reverse=False)
     return sortedd
