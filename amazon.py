ceb64b874eebb987b495dd15a19dbec405d6a371
koprty <your_email@example.com>
2014-11-10 22:12:43 -0500

    amazon api access key error

--- amazon.py
+++ amazon.py
@@ -1,4 +1,8 @@
 from amazonproduct import API
+
+api = API(locale='us')
+items = api.item_search('Books', Publisher="O'Reilly")
+'''
 api = API(locale='de')
 
 # get all books from result set and
@@ -6,3 +10,4 @@ api = API(locale='de')
 for book in api.item_search('Books', Publisher='Galileo Press'):
     print '%s: "%s"' % (book.ItemAttributes.Author,
                         book.ItemAttributes.Title)
+'''
