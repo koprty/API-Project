f38abaa691b9aa4cd1fd414107abfc21bd006bf9
koprty <your_email@example.com>
2014-11-10 22:49:15 -0500

    amazon api success

--- amazon.py
+++ amazon.py
@@ -1,7 +1,16 @@
 from amazonproduct import API
+config ={
+    'access_key': 'AKIAIE3MWTF3ITAIP4OA',
+    'secret_key': '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',
+    'associate_tag': 'appr0c9-20',
+    'locale': 'us',
+    }
 
-api = API(locale='us')
+api = API(cfg=config)
 items = api.item_search('Books', Publisher="O'Reilly")
+for book in items:
+    print '%s: "%s"' % (book.ItemAttributes.Author,
+                        book.ItemAttributes.Title)
 '''
 api = API(locale='de')
 
