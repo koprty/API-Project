e988f9dd7be9a64e9ffe5fe442d873e2dfbe8201
koprty <your_email@example.com>
2014-11-11 17:30:43 -0500

    working with prices - need to fix sorting

--- amazon.py
+++ amazon.py
@@ -1,4 +1,5 @@
 from amazonproduct import API
+import operator
 config ={
     'access_key': 'AKIAIE3MWTF3ITAIP4OA',
     'secret_key': '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',
@@ -7,6 +8,7 @@ config ={
     }
 
 api = API(cfg=config)
+
 def booksearch ():
     items = api.item_search('Books', Publisher="O'Reilly")
     for book in items:
@@ -20,41 +22,20 @@ def blendedsearch(searchwords):
     products = []
     for i in items:
         ids.append(i.ASIN)
-    print ids
     for asin in ids:
         product = api.item_lookup(str(asin), ResponseGroup="ItemAttributes")
-        productprice = api.item_lookup(str(asin),ResponseGroup='OfferFull')
+        productprice = api.item_lookup(str(asin),ResponseGroup="OfferFull")
         
         name = product.Items.Item.ItemAttributes.Title
         price = productprice.Items.Item.OfferSummary.LowestNewPrice.FormattedPrice
+        print name, price
+
         d[name] = price
-    return d
+
+    sortedd=sorted(d.items(), key=operator.itemgetter(1), reverse=Fals)
+    return sortedd
         
-'''d[product.ItemAttributes.title]= product.ItemAttributes.title
-#d[product.title] = product.price_and_currency
-    print d'''
-#booksearch()
-
-<<<<<<< HEAD
-def code(searchWord):
-    pageNo = 1
-    result = api.item_search('All', Keywords=searchWord, ResponseGroup='Large', ItemPage=pageNo, MerchantId='All')
-    for item in result:
-        try:
-            print item.ItemAttributes.Title
-        except (UnicodeEncodeError):
-            pass
-    prices = api.item_lookup('All', Keywords=searchWord, ResponseGroup='Offers', ItemPage=pageNo, MerchantId='All')
-    for item in prices.Items.Item.Offers.Offer:
-        try:
-            print item.OfferListing.Price.FormattedPrice
-        except (UnicodeEncodeError):
-            pass
-=======
-blendedsearch("Bracelet")
->>>>>>> 8f59cb5e89b948eeda775d7bc9f2e3c62b0df7aa
-
-print blendedsearch("fluffy")
+print blendedsearch("bracelet")
         
 '''
 api = API(locale='de')
